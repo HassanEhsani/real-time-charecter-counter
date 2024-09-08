558694d298f1a982a0f96f0f8615e5c7ec8f6c40
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-09 00:01:46 +0300

    Done!

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
- const textareaEl = document.getElementById("textarea");
+const textareaEl = document.getElementById("textarea");
 
- const totalCounterEl = document.getElementById("total-counter")
+const totalCounterEl = document.getElementById("total-counter")
 
- const remainingCounterEl = document.getElementById("remaining-counter")
+const remainingCounterEl = document.getElementById("remaining-counter")
 
- textareaEl.addEventListener("keyup",()=>{
+textareaEl.addEventListener("keyup", () => {
     updateCounter()
- });
-
- function updateCounter(){
+});
+updateCounter();
+function updateCounter() {
     totalCounterEl.innerText = textareaEl.value.length
     remainingCounterEl.innerText = textareaEl.getAttribute("maxlength") - textareaEl.value.length;
- } 
+} 
